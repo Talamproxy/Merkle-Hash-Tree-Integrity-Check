5ef29b97dd69b73a80ef382c742d8fc39be33be9
Talamproxy <kiptalambrian3@gmail.com>
2022-04-06 12:16:20 -0400

    Read the files in binary file and encoding

--- merkle.py
+++ merkle.py
@@ -1,1 +1,14 @@
-#! /bin/python3
+#!/usr/bin/python3
+import glob, os
+from hashlib import md5, sha1
+
+
+class Merkle_tree:
+    os.chdir("/home/kali/Desktop/merkle/Merkle-Hash-Tree-Integrity-Check")
+    hashArray = [] 
+    for file in glob.glob("*.txt"):
+        binary_file=open(file, 'rb').read()
+        hash=md5(binary_file).hexdigest()
+        hashArray.append(hash)
+        print(hashArray)
+    
